c49c9f1b696276d6a94e30e19b76bc7159b0fef6
MarleyDey <marleydey11@gmail.com>
2020-04-27 12:56:20 +0100

    Attempted decoder

--- src/me/marley/rle/java/Encoder.kt
+++ src/me/marley/rle/java/Encoder.kt
@@ -29,7 +29,47 @@ class Encoder(private var input: InputParser) {
     }
 
     fun decode() : String {
-        return ""
-    }
+        val resultBuilder =  StringBuilder()
+        val inputString = input.getInput()
 
+        if (inputString.isEmpty()) throw Exception("Input cannot be empty.")
+
+        var i = 0
+        while (true){
+            if (inputString.length <= i)break;
+
+            var c = inputString[i]
+            var a = 0;
+
+            if (c.isDigit()){
+                var shift = 0
+                while (true){
+
+                    if (inputString.length >= i + shift || !inputString[i + shift].isDigit()){
+                        var ac = ""
+
+                        println("Shift: " + shift)
+                        for (s in (i + (shift - 1)) downTo i){
+                            ac += inputString[s]
+                            println("Adding: " + inputString[s])
+                            break
+                        }
+
+                        a = ac.toInt()
+                        println("Amount: $a")
+                        i += shift
+                    }
+                    if (shift++ >= inputString.length) break
+                    println("Shift after: " + shift)
+
+                }
+                resultBuilder.append(("" + inputString[i - shift]).repeat(a))
+                continue
+            }
+
+            i++;
+        }
+
+        return resultBuilder.toString();
+    }
 }
